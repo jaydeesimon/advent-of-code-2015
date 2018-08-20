67191353d0e78f58667cd61bcd692161b2731ab1
Jeffrey Simon <jdsimon@gmail.com>
2018-08-20 09:21:39 -0400

    Day 12

--- src/advent_of_code_2015/day12.clj
+++ src/advent_of_code_2015/day12.clj
@@ -3,23 +3,38 @@
             [clojure.walk :as walk]
             [advent-of-code-2015.utils :as utils]))
 
+(defn has-red? [m]
+  (some (fn [[k v]]
+          (or (= k "red") (= v "red")))
+        m))
 
-(comment
 
-  ;; Part 1
-  (let [s (utils/input "day12.txt")
-        json (json/parse-string s)
-        sum (atom 0)]
+(defn sum-numbers [m]
+  (let [sum (atom 0)]
     (walk/postwalk (fn [form]
                      (if (number? form)
                        (do
                          (swap! sum #(+ % form))
                          form)
                        form))
-                   json)
-    @sum)
+                   m)
+    @sum))
 
-  ;; TODO: Part 2
-  
-  )
+
+(comment
+
+  ;; Part 1
+  (let [s (utils/input "day12.txt")
+        json (json/parse-string s)]
+    (sum-numbers json))
+
+  ;; Part 2
+  (let [s (utils/input "day12.txt")
+        json (json/parse-string s)
+        filtered-json (walk/postwalk (fn [form]
+                                      (if (and (map? form) (has-red? form))
+                                        {}
+                                        form))
+                                    json)]
+    (sum-numbers filtered-json)))
 
